9ff0914c2c1b419df644912a799146a248e2121a
Archana Pradhan <mail.archanapradhan@gmail.com>
2020-04-22 19:01:23 +1200

    Dynamically outputting the ingredients based on the props passed into Burger component[C

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,27 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = (props) => {
+  // const transformedIngredients = Object.keys(props.ingredients).map()
+  let transformedIngredients = Object.entries(props.ingredients)
+    .map(([igKey, igValue]) => {
+      console.log(igKey + ": key and val :" + igValue + ". ");
+      return [...Array(igValue)].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce(
+      (accumulator, currentValue) => accumulator.concat(currentValue),
+      []
+    );
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
-      <BurgerIngredient type="cheese"></BurgerIngredient>
-      <BurgerIngredient type="meat"></BurgerIngredient>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"></BurgerIngredient>
     </div>
   );
